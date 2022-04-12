b063cda9e2b50e097a73f8ce5c298a4cc67bdf26
Olivier Van hamme <nicefiction@gmail.com>
2022-04-12 20:13:25 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Olivier Van hamme on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
